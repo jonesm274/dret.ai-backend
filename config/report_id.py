a075649e3f394f4ec812045547d2f65b82827de2
unknown <mjones@dret.co.uk>
2025-07-09 23:01:33 +0100

    updates

--- config/report_id.py
+++ config/report_id.py
@@ -1,13 +1,81 @@
 import os
 
 POWERBI_REPORTS = {
-    "report001": {
+    "dret-academy-report": {
+        "name": "DRET Academy Report",
+        "report_id": "301f9926-b7ae-4e86-b7d2-5aaa647251df",
+        "dataset_id": "06fcd37c-a904-4151-8f41-32b039399235",
+        "group_id": "829bdcb0-220e-46d0-9a2f-821c725a8490"
+    },
+    "dret-education-team-report": {
+        "name": "DRET Education Team Report",
+        "report_id": "ddb8f3ca-1fdd-41ff-a6ad-41ea29fd80be",
+        "dataset_id": "afce48ee-4864-4ead-a92c-64d5e0ba187c",
+        "group_id": "829bdcb0-220e-46d0-9a2f-821c725a8490"
+    },
+    "dret-pupil-profile": {
+        "name": "DRET Pupil Profile",
         "report_id": "632a88cf-74f0-427a-a98f-2e6cfc03da8e",
         "dataset_id": "5312b1a7-8509-429b-a907-84448e774319",
         "group_id": "829bdcb0-220e-46d0-9a2f-821c725a8490"
     },
-    "report002": {
+    "mat-comparison-analysis-ks2": {
+        "name": "MAT Comparison Analysis KS2",
+        "report_id": "74be3885-cceb-4588-90b9-f8e6f4ee59b1",
+        "dataset_id": "8c19f8c5-bbc8-4b75-9399-7cfd0b707086",
+        "group_id": "829bdcb0-220e-46d0-9a2f-821c725a8490"
+    },
+    "mat-comparison-analysis-ks4": {
+        "name": "MAT Comparison Analysis KS4",
+        "report_id": "2a339b99-d4dd-4298-9bcd-31424a643e49",
+        "dataset_id": "b5d8ffde-618d-45da-a821-187d4e8013e1",
+        "group_id": "829bdcb0-220e-46d0-9a2f-821c725a8490"
+    },
+    "bromcom-attendance": {
+        "name": "Bromcom Attendance",
+        "report_id": "4149e102-e90e-41e2-91cc-70b09db3c7b9",
+        "dataset_id": "fa0c660b-fb94-4f49-ad1d-9de8a60b075c",
+        "group_id": "c526f3f7-eeac-4ca0-a178-e502c9488f23"
+    },
+    "bromcom-eyf-stage-assessment": {
+        "name": "Bromcom EYF Stage Assessment",
+        "report_id": "1cbfd01d-11cb-4d33-b7a9-72891455cb0c",
+        "dataset_id": "fa0c660b-fb94-4f49-ad1d-9de8a60b075c",
+        "group_id": "c526f3f7-eeac-4ca0-a178-e502c9488f23"
+    },
+    "bromcom-formative-assessment": {
+        "name": "Bromcom Formative Assessment",
         "report_id": "fa354e20-d998-4b49-bcb8-a7858837baf7",
+        "dataset_id": "fa0c660b-fb94-4f49-ad1d-9de8a60b075c",
+        "group_id": "c526f3f7-eeac-4ca0-a178-e502c9488f23"
+    },
+    "bromcom-ks1-ks2-assessment": {
+        "name": "Bromcom KS1 KS2 Assessment",
+        "report_id": "0ac1322e-8622-4fd9-93e9-1120a707c314",
+        "dataset_id": "fa0c660b-fb94-4f49-ad1d-9de8a60b075c",
+        "group_id": "c526f3f7-eeac-4ca0-a178-e502c9488f23"
+    },
+    "bromcom-ks4-performance": {
+        "name": "Bromcom KS4 Performance",
+        "report_id": "db0e1c93-c7fc-4f03-80fa-b6d6a53417a2",
+        "dataset_id": "fa0c660b-fb94-4f49-ad1d-9de8a60b075c",
+        "group_id": "c526f3f7-eeac-4ca0-a178-e502c9488f23"
+    },
+    "bromcom-mat-performance-overview": {
+        "name": "Bromcom MAT Performance Overview",
+        "report_id": "67ddac5f-2e36-48f1-9437-212db4337c92",
+        "dataset_id": "fa0c660b-fb94-4f49-ad1d-9de8a60b075c",
+        "group_id": "c526f3f7-eeac-4ca0-a178-e502c9488f23"
+    },
+    "bromcom-ks1-ks2-tracker": {
+        "name": "Bromcom KS1 KS2 Tracker",
+        "report_id": "b1883672-f69b-4400-b4c2-c5f742805bbc",
+        "dataset_id": "fa0c660b-fb94-4f49-ad1d-9de8a60b075c",
+        "group_id": "c526f3f7-eeac-4ca0-a178-e502c9488f23"
+    },
+    "bromcom-suspensions-exclusions": {
+        "name": "Bromcom Suspensions & Exclusions",
+        "report_id": "a1f9863b-1b8d-49b3-bc6d-fcc09b1c2df1",
         "dataset_id": "fa0c660b-fb94-4f49-ad1d-9de8a60b075c",
         "group_id": "c526f3f7-eeac-4ca0-a178-e502c9488f23"
     }
