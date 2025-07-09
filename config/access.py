46560996b4da13d1312e79ce5049914a0fbd8992
unknown <mjones@dret.co.uk>
2025-07-09 23:33:24 +0100

    updates

--- config/access.py
+++ config/access.py
@@ -1,3 +1,17 @@
 GROUP_ACCESS = {
-    "fc33b641-1349-4006-96a3-85ea93af8c0f": ["report001", "report002"]
+    "fc33b641-1349-4006-96a3-85ea93af8c0f": [
+        "dret-academy-report",
+        "dret-education-team-report",
+        "dret-pupil-profile",
+        "mat-comparison-analysis-ks2",
+        "mat-comparison-analysis-ks4",
+        "bromcom-attendance",
+        "bromcom-eyf-stage-assessment",
+        "bromcom-formative-assessment",
+        "bromcom-ks1-ks2-assessment",
+        "bromcom-ks4-performance",
+        "bromcom-mat-performance-overview",
+        "bromcom-ks1-ks2-tracker",
+        "bromcom-suspensions-exclusions",
+    ]
 }
